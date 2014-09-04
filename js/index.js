c05823ddce1a0186208b26621c1a16708ec46acd
anamary <1225733380@qq.com>
2014-09-04 15:44:22 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
     projectop = function() {
         $(".useroplist").click(function() {
-            alert($(this).children('span').eq(0).text());
+            var _this = $(this);
+            var nexturl = _this.data('traget');
+            $(".msg").css({
+                'display': 'inline-block'
+            });
+            $.ajax({
+                type: 'GET',
+                url: nexturl,
+                success: function(data, textStatus, jqXHR) {
+                    $(".msg").css({
+                        'display': 'none'
+                    });
+                    console.log(data);
+                },
+                error: function(data, textStatus, jqXHR) {
+                    adminlogin();
+                },
+                dataType: 'json'
+            });
         });
     };
     checkCookie(adminlogin, projectop);
