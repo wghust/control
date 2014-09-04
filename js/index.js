02f87cdd825ea2172c105cf624c6de45759c341c
anamary <1225733380@qq.com>
2014-09-04 14:00:19 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    checkCookie(adminlogin, projectop);
-
     projectop = function() {
         $(".useroplist").click(function() {
             alert($(this).children('span').eq(0).text());
         });
     };
+    checkCookie(adminlogin, projectop);
 });
